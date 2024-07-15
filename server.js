1bc33070e5e30a759eed6ff44eb15796c1121d52
sebasSerrano12 <se.serrano12@gmail.com>
2024-07-15 09:18:26 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app =  express()
+const PORT = 8000
+
+const rappers = {
+    '21 savage':{
+    'age': 29,
+    'birthName': 'Sheyaa Bin Abraham-Joseph',
+    'birthLocation': 'London, England'
+    },
+    'chance the rapper':{
+    'age': 28,
+    'birthName': 'Chancellor Bennet',
+    'birthLocation': 'Chicago, Illinois'
+    },
+    'dylan':{
+    'age': 28,
+    'birthName': 'Dylan',
+    'birthLocation': 'Dylan'
+    },
+}
+
+
+app.get('/', (request, response)=>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:rapperName', (request, response)=>{
+    const rappersName = request.params.rapperName.toLowerCase()
+    if(rappers[rappersName]){
+        response.json(rappers[rappersName])
+    }else {
+        response.json(rappers['dylan'])
+    }
+})
+
+app.listen(PORT, ()=>{
+    console.log(`The server is running on ${PORT} better go catch it!!!`)
+})
